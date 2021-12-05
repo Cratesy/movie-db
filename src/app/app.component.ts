1be3dca80e02ef605089c84271be6ecb9c2cf635
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-12-05 17:27:57 +0000

    added search query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movie-db';
+  input: string = '';
+  response: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  search() {
+    this.http
+      .get(
+        'https://api.themoviedb.org/3/search/movie?api_key=5ba3487f4d728b9197be75649671030c&query=' +
+          this.input
+      )
+      .subscribe((response) => {
+        this.response = response;
+        console.log(this.response);
+      });
+  }
 }
